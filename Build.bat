e2b5d8f688bb14d599762e736db14adabb1f0381
Philip Edwards <philip@gdcorner.com>
2020-03-07 22:44:37 +1100

    Added missing tkinter libraries to python runtime.

--- Build.bat
+++ Build.bat
@@ -15,6 +15,8 @@ mkdir %OUT_DIR%\WindowsNoEditor\%PROJECT_NAME%\Content\Scripts
 xcopy /s %PROJECTDIR%\Content\Scripts %OUT_DIR%\WindowsNoEditor\%PROJECT_NAME%\Content\Scripts\ /EXCLUDE:XCopyExclude.txt
 ::Copy Pillow
 xcopy /s %PROJECTDIR%\Redist\PIL %OUT_DIR%\WindowsNoEditor\%PROJECT_NAME%\%PROJECT_NAME%\Content\Scripts\PIL\ /EXCLUDE:XCopyExclude.txt
+::Copy TKinter
+xcopy /s %PROJECTDIR%\Redist\tkinter %OUT_DIR%\WindowsNoEditor\%PROJECT_NAME%\%PROJECT_NAME%\Content\Scripts\tkinter\ /EXCLUDE:XCopyExclude.txt
 ::Copy Python
 xcopy /s %PROJECTDIR%\Redist\Python\* %OUT_DIR%\WindowsNoEditor\%PROJECT_NAME%\Binaries\Win64\ /EXCLUDE:XCopyExclude.txt
 
