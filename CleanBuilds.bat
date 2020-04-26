a95fa2e18423f6a6653c68bc851edfbadac5f932
Philip Edwards <philip@gdcorner.com>
2020-04-26 13:26:54 +1000

    Stopped cleanWS requirement, with a script to delete intermediate files

---
+++ CleanBuilds.bat
@@ -0,0 +1,20 @@
+set PROJECTDIR=%WORKSPACE%
+if ["%WORKSPACE%"]==[""] (
+    set PROJECTDIR=%~dp0
+)
+
+set UE4ROOT="C:\Unreal\UE_4.23"
+set PROJECT_NAME=RainbowRedux
+set OUT_DIR=%PROJECTDIR%\Packaged
+set BUILD_PROFILE=Development
+cd %~dp0
+
+RMDIR /s /q %OUT_DIR%
+RMDIR /s /q %PROJECTDIR%\Saved
+RMDIR /s /q %PROJECTDIR%\Intermediate
+RMDIR /s /q %PROJECTDIR%\Plugins\UnrealEnginePython
+RMDIR /s /q %PROJECTDIR%\Content\Scripts
+
+del /f /q %PROJECTDIR%\Build\*.pdb
+del /f /q %PROJECTDIR%\Build\*.dll
+del /f /q %PROJECTDIR%\Build\*.exe
