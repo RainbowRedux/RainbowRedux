9b4af6681e1f4db57799e1917df7da31112e6e38
Philip Edwards <philip@gdcorner.com>
2021-06-12 18:06:00 +1000

    Update environment variables to ensure network drives get mapped, and remove some problematic quotes

--- EnvVars.bat
+++ EnvVars.bat
@@ -3,7 +3,9 @@ if ["%WORKSPACE%"]==[""] (
     set PROJECTDIR=%~dp0
 )
 
-set UE4ROOT="S:\Engines\UE_4.23"
+::Need to ensure network drives are mapped to have access to S:
+call c:\ConnectNetworkDrives.bat
+set UE4ROOT=S:\Engines\UE_4.23
 set PROJECT_NAME=RainbowRedux
 set OUT_DIR=%PROJECTDIR%\Packaged
 set BUILD_PROFILE=Development
